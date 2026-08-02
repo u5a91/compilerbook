4f12decb678e395193ed727eb4f92f0769f028cc
u5a91 <fruitmix246@gmail.com>
2026-08-03 03:09:39 +0900

    Add step5: Support multiplication, division, and parentheses

---
+++ step5/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+assert() {
+    expected="$1"
+    input="$2"
+
+    ./9cc "$input" > tmp.s
+    cc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+assert 0 0
+assert 42 42
+assert 0 "1+2-3"
+assert 6 "1 + 2 + 3"
+assert 6 "(1+2)+3"
+assert 6 "1*2*3"
+assert 88 "(12/3+4)*(5+6)"
+assert 2 "8/2/2"
